54fce19982698c58c8bedfe63d629b22adabebbb
진환 정 <joung.jinhawn@gmail.com>
2023-11-09 17:55:14 +0900

    feat(model/Menu.java) : 메뉴 모델 작성

---
+++ src/main/java/christmas/model/Menu.java
@@ -0,0 +1,13 @@
+package christmas.model;
+
+public class Menu {
+    private final String menuType;
+    private final String menuName;
+    private final int menuPrice;
+    
+    public Menu(String menuType, String menuName, int menuPrice) {
+        this.menuType = menuType;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+    }
+}
