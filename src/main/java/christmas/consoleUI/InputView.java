7ae522f4f79381837a0b565f744c77c962c84349
진환 정 <joung.jinhawn@gmail.com>
2023-11-09 19:42:04 +0900

    feat(consoleUI/InputView.java) : 식당 방문 날짜 입력 파트 구현

--- src/main/java/christmas/consoleUI/InputView.java
+++ src/main/java/christmas/consoleUI/InputView.java
@@ -1,5 +1,30 @@
 package christmas.consoleUI;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+
+        int date = convertStringToInt(input);
+        validateDate(date);
+
+        return date;
+    }
+
+    private int convertStringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("올바른 숫자 형식이 아닙니다!");
+        }
+    }
+
+
+    private void validateDate(int date) {
+        if (1 > date || date > 31) {
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해주세요.");
+        }
+    }
 }
