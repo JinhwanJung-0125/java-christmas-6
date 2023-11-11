b06a0d1eb338580e27fb607a29a1c49095cbad57
FGWDR <joung.jinhawn@gmail.com>
2023-11-12 00:44:39 +0900

    feat(consoleUI/OutputView.java) : 첫 메시지와 이벤트 혜텍 미리 보기 메시지 출력 파트 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -9,17 +9,25 @@ import christmas.model.UserOrders;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printFirstMessage();
         int date = InputView.readDate();
         UserOrders orders = InputView.readMenu();
         Item givenItem = Discount.getGivenEvent(orders);
 
-        System.out.println(date);
+        OutputView.printResultMessage(date);
+        System.out.println();
         OutputView.printOrders(orders);
+        System.out.println();
         OutputView.printBeforeTotalPrice(orders);
+        System.out.println();
         OutputView.printGivenItem(givenItem);
+        System.out.println();
         OutputView.printDiscountHistory(date, orders);
+        System.out.println();
         OutputView.printTotalDiscount(date, orders);
+        System.out.println();
         OutputView.printAfterTotalPrice(date, orders);
+        System.out.println();
         OutputView.printBadge(date, orders);
     }
 }
--- src/main/java/christmas/consoleUI/OutputView.java
+++ src/main/java/christmas/consoleUI/OutputView.java
@@ -9,6 +9,14 @@ import christmas.model.UserOrders;
 import java.util.HashMap;
 
 public class OutputView {
+    public static void printFirstMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printResultMessage(int date) {
+        System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
     public static void printOrders(UserOrders userOrders) {
         HashMap<String, Integer> orders = userOrders.getOrders();
 
